62a5adabcf5d0f6029c573e68a44caf2152c3676
bolshoib <bolshoib@gmail.com>
2017-01-09 22:06:05 +0100

    bolshoib add files

--- cherry.py
+++ cherry.py
@@ -1,11 +1,22 @@
 import cherrypy
-
-
-class HelloWorld(object):
-    @cherrypy.expose
-    def index(self):
-        return "Hello world!"
-
-
-if __name__ == '__main__':
-    cherrypy.quickstart(HelloWorld())
+ 
+class Bienvenue(object):
+  def index(self):
+      # Formulaire demandant son nom à l'utilisateur :
+      return '''
+      <form action="salutations" method="GET">
+      Bonjour. Quel est votre nom ?
+      <input type="text" name="nom" />
+      <input type="submit" value="OK" />
+      </form>
+      '''
+  index.exposed = True
+ 
+  def salutations(self, nom =None):
+      if nom:	      # Accueil de l'utilisateur :
+        return "Bonjour, {}, comment allez-vous ?".format(nom)
+      else:	    # Aucun nom n'a été fourni :
+        return 'Veuillez svp fournir votre nom <a href="/">ici</a>.'
+  salutations.exposed = True
+ 
+cherrypy.quickstart(Bienvenue(), config ="tutoriel.conf")
